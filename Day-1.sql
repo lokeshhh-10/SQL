8c80ada1682d6a158eb341d963c8bba29f8d26cc
lokesh <lokesh95664@gmail.com>
2024-12-01 21:45:15 +0530

    ....

--- Day-1.sql
+++ Day-1.sql
@@ -47,3 +47,50 @@ SELECT * FROM Student WHERE FIRST_NAME LIKE '%a';
 
 SELECT * FROM Student WHERE FIRST_NAME LIKE '%a' AND length(FIRST_NAME) == 6 ;
 
+-- 13. Write an SQL query to print details of the Students whose GPA lies between 9.00 and 9.99.
+
+SELECT * FROM Student WHERE GPA BETWEEN 9.00 AND 9.99
+
+-- 14. Write an SQL query to fetch the count of Students having Major Subject ‘Computer Science’.
+
+SELECT Major, COUNT(*) FROM Student WHERE Major = "Compter Science"
+
+-- 15. Write an SQL query to fetch Students full names with GPA >= 8.5 and <= 9.5.
+
+SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS FULL_NAME FROM Student WHERE GPA BETWEEN 8.5 and 9.5;
+
+-- 16. Write an SQL query to fetch the no. of Students for each MAJOR subject in the descending order.
+
+SELECT MAJOR, COUNT(MAJOR) from Student group by MAJOR order by COUNT(MAJOR) DESC;
+
+-- 17. Display the details of students who have received scholarships, including their names, scholarship amounts, and scholarship dates.
+
+SELECT 
+    Student.FIRST_NAME,
+    Student.LAST_NAME,
+    Scholarship.SCHOLARSHIP_AMOUNT,
+    Scholarship.SCHOLARSHIP_DATE
+FROM 
+    Student
+INNER JOIN 
+    Scholarship ON Student.STUDENT_ID = Scholarship.STUDENT_REF_ID;
+
+-- 18. Write an SQL query to show only odd rows from Student table.
+
+SELECT * FROM Student WHERE Student_id % 2 != 0;
+
+-- 19. Write an SQL query to show only even rows from Student table.
+
+SELECT * FROM Student WHERE Student_id % 2 = 0;
+
+-- 20. List all students and their scholarship amounts if they have received any. If a student has not received a scholarship, display NULL for the scholarship details.
+
+SELECT 
+    Student.FIRST_NAME,
+    Student.LAST_NAME,
+    Scholarship.SCHOLARSHIP_AMOUNT,
+    Scholarship.SCHOLARSHIP_DATE
+FROM 
+    Student
+LEFT JOIN 
+    Scholarship ON Student.STUDENT_ID = Scholarship.STUDENT_REF_ID;
